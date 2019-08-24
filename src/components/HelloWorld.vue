a7a3df7757b26f7e42448ba56a8caf7acdc1d812
sudh-17 <395338095@qq.com>
2019-08-24 23:22:10 +0800

    index

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://sudh-17.github.io/webpack-todomvc/dist/index.html" target="_blank" rel="noopener">javascript原生版本(webpack 打包)</a></li>
+      <li><a href="https://sudh-17.github.io/vue-antd-todomvc/dist/index.html#/home" target="_blank" rel="noopener">基于vue框架的版本</a></li>
+      <li><a href="https://sudh-17.github.io/ant-design-todomvc/dist/index.html#/home" target="_blank" rel="noopener">基于vue + ant-design-vue框架的版本</a></li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
   </div>
 </template>
 
@@ -49,10 +29,11 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
+  margin: 10px 0;
 }
 a {
   color: #42b983;
+  font-size: 25px;
 }
 </style>
